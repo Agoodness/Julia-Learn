a5a97d0f7aa9c6efbde408a382f5bda15924f3e7
Goodness <goodnessadedeji7@gmail.com>
2024-05-01 17:58:07 +0100

    Julia Lesson 01_05_24

---
+++ JuliaCourse/Variables.jl
@@ -0,0 +1,57 @@
+x = 42
+
+x + 12; y = 15; z = 18;
+
+y = x^3 + 3*x
+
+x = "Programming in Julia"
+
+z = 3.142   
+typeof(z)
+z = "This is a string"
+typeof(z)
+
+_abc = 213 
+println(_abc)
+
+α = 15  #\alpha TAB
+β = 15  #\beta TAB 
+
+α * β 
+
+β₀ = 3  #\beta TAB \_0 TAB
+β₁ = 4  #\beta TAB |_1 TAB
+
+α¹ = 13 #\alpha TAB \^1 TAB
+
+π   #\pi 
+
+# For multiline comment use 
+#= this 
+is still a comment  =#
+
+# Assigning values to multiple varibles at once 
+a, b, c = 4, 7, 9;
+a
+b
+c
+
+# Switching values of varibles 
+a, b = b, a
+a
+b
+
+d =  b
+b = a
+a = a
+
+# To check what kind of variable type 
+(7 + 8) ::Int
+(π + ℯ) ::Float64
+(2.5 * 5) ::Int
+
+x = 13
+typeof(x)
+x = "thirteen"
+typeof(x)
+
