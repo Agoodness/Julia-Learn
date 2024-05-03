033979920d6b35e2a97e92d982a79bb735aea0e8
Goodness <goodnessadedeji7@gmail.com>
2024-05-03 11:29:51 +0100

    Completed the CharString Lesson

--- Variables/CharString.jl
+++ Variables/CharString.jl
@@ -10,4 +10,46 @@ str = "Julia String"
 longstr = """Another Julia String"""
 typeof(longstr)
 
-str
+str = "Julia Language"
+str[1] 
+str[14]
+
+str[begin]
+    
+str[end]
+
+str[end - 1]
+str[end ÷ 2]
+
+str[3:5]
+firstindex(str)
+lastindex(str)
+length(str)
+
+# For inicode characters
+newstr = "αβσ"
+firstindex(newstr)
+lastindex(newstr)
+
+newstr[1]
+newstr[2]
+newstr[3]
+# We are getting error because not all characters are save in 1byte, α and β are save with two bytes
+
+c = "∞ α a Σ"
+length(c)
+lastindex(c)
+
+ncodeunits('→')
+
+# Concatenate two strings
+str1 = "Julia programming"
+str2 = "Is the best."
+string(str1, str2)
+
+str1 * " " *str2
+"julia "^4
+
+# To interapolate
+x = 4; y = 5;
+"First number is $x, the second number is $y  and their sum if $(x + y)"
