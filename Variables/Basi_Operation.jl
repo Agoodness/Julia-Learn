4ed0a92b0546fa49201d8d983a7b78d6cbd898d0
Goodness <goodnessadedeji7@gmail.com>
2024-05-07 19:49:03 +0100

    Added basic operation lesson

---
+++ Variables/Basi_Operation.jl
@@ -0,0 +1,55 @@
+5 + 7
+14 - 6 
+3.14 * 2.78
+67 / 13
+ans 
+
+ans^2
+
+x = 12; y = 23;
+3x + 5y
+
+x, y = 3, 5
+3x + 5y 
+
+4(5x + 7y)
+
+15 ÷ 6   # \div TAB
+
+div(15, 6)
+div(6.5, 2)
+
+4^3 
+0.25^0.5
+
+# In Julia, operators are functions 
+4+5 
++(4, 5)
+^(4 ,3)
+
+5\15
+
+sqrt(16)
+√16       # \sqrt TAB 
+
+cbrt(64)
+∛64
+
+rem(28, 5)
+
+a = 7.8; b = 3.6;
+a == b
+a != b
+a ≠ b
+
+c = [1, 2, 3]
+d = c
+e = deepcopy(c) # This function takes the vector to another memory operation
+
+c == d
+c == e 
+c === e
+
+c ≡ e # \equiv TAB
+a ≤ b # \leg TAB 
+a ≥ b # \geq TAB 
