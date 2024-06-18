866613a8dd23837ba40b6fc4a1bf66c339a7f016
Goodness <goodnessadedeji7@gmail.com>
2024-06-18 11:25:51 +0100

    Added Mad_Libs project

---
+++ 12_Test_Projects/Mad_Libs.jl
@@ -0,0 +1,14 @@
+
+println("Please Enter the subject")
+Subject::String = readline()
+
+println("Please Enter the Verb here")
+Verb::String = readline()
+
+println("Please Enter the Object here")
+Object::String = readline()
+
+println("Please Enter the Complementary message here")
+Complement::String = readline()
+
+println("$Subject $Verb $Object $Complement")
